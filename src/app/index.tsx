ebf1163c6e8a277f2467adaafc47af95fe5fbd91
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-07-21 21:05:51 -0300

    style: estilização alterada && feat: salvar as tasks e remover

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,18 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
 import { Header } from "../components/header";
 import { AddNotes } from "../components/addNotes";
 import "../styles/main.css";
+import { NotesProvider } from "../contexts/notes";
+import Notes from "../components/notes";
 
 export default function Index() {
   return (
-    <View className="flex-1 flex flex-col bg-gray-800">
-      <Header />
-      <AddNotes />
-    </View>
+    <NotesProvider>
+      <View className="flex-1 flex flex-col bg-gray-900 w-full">
+        <Header />
+        <AddNotes />
+        <Notes />
+      </View>
+    </NotesProvider>
   );
 }
--- src/components/addNotes/index.tsx
+++ src/components/addNotes/index.tsx
@@ -1,20 +1,50 @@
-import { View, Text, TouchableOpacity } from "react-native";
-import React, { useState } from "react";
-import { Feather } from "@expo/vector-icons";
+import { View, Text, TextInput, Pressable } from "react-native";
+import React from "react";
+import { useNotesContext } from "@/src/contexts/notes";
 
 export function AddNotes() {
+  const { task, setTask, addTask, edited, setAllTasks, allTasks, setEdited } =
+    useNotesContext();
+
+  const handleAddTask = (task: string) => {
+    if (!task) return;
+    addTask(task);
+    setTask("");
+  };
+
+  const handleEditTask = (task: string) => {
+    if (!task) return;
+    setEdited(false);
+  };
+
+  // const taskIndex = allTasks.findIndex((t) => t === task);
+  // console.log(taskIndex)
+
   return (
-    <View className="flex-1 w-full flex flex-col items-center">
-      <View className="w-3/4 flex flex-row items-center justify-between">
-        <Text className="text-white text-6xl">Anotações</Text>
-        <Feather
-          name="plus-square"
-          size={42}
-          color="white"
-          onPress={() => alert("ta clicado")}
-          className="rounded "
-        />
-      </View>
+    <View className="w-full flex flex-row items-center px-4 gap-4">
+      <TextInput
+        value={task}
+        placeholder="Adicione uma nova tarefa"
+        onChangeText={setTask}
+        className="w-3/4 bg-gray-700 rounded-lg p-4 text-white placeholder:text-white"
+      />
+      <Pressable>
+        {edited ? (
+          <Text
+            className="bg-white p-4 rounded-lg "
+            onPress={() => handleEditTask(task)}
+          >
+            Editar
+          </Text>
+        ) : (
+          <Text
+            className="bg-white p-4 rounded-lg "
+            onPress={() => handleAddTask(task)}
+          >
+            Adicionar
+          </Text>
+        )}
+      </Pressable>
     </View>
   );
 }
